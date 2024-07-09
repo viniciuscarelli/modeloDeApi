04dc9c72793d6ab7325cc59a68bef986eba89f84
viniciuscarelli <viniciuscarelli5@gmail.com>
2024-07-08 22:34:39 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,10 +10,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Configurando a conexão com o banco de dados MySQL
 const db = mysql.createConnection({
-    host: 'esi.h.filess.io',
-    user: 'eudora_hatfought',
-    password: '1871cc0c1003b3dab30384527bf6efb162e0fbc5',
-    database: 'eudora_hatfought'
+    host: 'esi.h.filess.io', // Endereço do servidor do banco de dados
+    user: 'eudora_hatfought', // Usuário do banco de dados
+    password: '1871cc0c1003b3dab30384527bf6efb162e0fbc5', // Senha do banco de dados
+    database: 'eudora_hatfought' // Nome do banco de dados
 });
 
 // Conectando ao banco de dados
@@ -28,25 +28,13 @@ db.connect((err) => {
 app.post('/submit-form', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    const redirectUrl = 'http://127.0.0.1:5500/eudoraEndereco/index.html'; // Substitua pelo endereço web desejado
 
     const sql = 'INSERT INTO usuarios (email, password) VALUES (?, ?)';
     db.query(sql, [email, password], (err, result) => {
         if (err) {
             return res.status(500).send('Erro ao inserir dados no banco de dados');
         }
-
-        // Redireciona o usuário para a URL especificada com os dados no corpo da requisição
-        const form = `
-            <form id="redirectForm" method="POST" action="${redirectUrl}">
-                <input type="hidden" name="email" value="${email}" />
-                <input type="hidden" name="password" value="${password}" />
-            </form>
-            <script type="text/javascript">
-                document.getElementById('redirectForm').submit();
-            </script>
-        `;
-        res.send(form);
+        res.status(200).send('Dados salvos com sucesso!');
     });
 });
 
